8989b9db69a7a42a53bb3cee2872504bde015005
Tomoya Tanaka <tomoya113.code@gmail.com>
2021-06-27 00:49:38 +0900

    implement TodoDetailView

--- commit/commit/Modules/TodoDetailModule/View/TodoDetailView.swift
+++ commit/commit/Modules/TodoDetailModule/View/TodoDetailView.swift
@@ -8,8 +8,20 @@
 import SwiftUI
 
 struct TodoDetailView: View {
+	@State private var title = ""
+	@State private var detail = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+		Form {
+			Section(header: Text("タスク名")) {
+				TextField("タスク名", text: $title)
+			}
+			Section(header: Text("状態")) {
+				TextField("タスク名", text: $title)
+			}
+			Section(header: Text("説明"), footer: Text("任意の文字列を設定することが出来ます。\nDoneは") + Text(Image(systemName: "checkmark.square.fill")) + Text("と表示されます。")) {
+				TextField("任意の説明", text: $detail)
+			}
+		}
     }
 }
 
