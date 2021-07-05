6afbcafad5e69f74394a12a8939ab4722641c342
Tomoya Tanaka <tomoya113.code@gmail.com>
2021-07-06 03:42:53 +0900

    add new files, NormalTodoForm.swift and SpreadSheetTodoForm.swift

---
+++ commit/commit/Modules/TodoAddModule/View/SpreadSheetTodoForm.swift
@@ -0,0 +1,20 @@
+//
+//  SpreadTodoForm.swift
+//  commit
+//
+//  Created by Tomoya Tanaka on 2021/07/06.
+//
+
+import SwiftUI
+
+struct SpreadSheetTodoForm: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SpreadSheetTodoForm_Previews: PreviewProvider {
+    static var previews: some View {
+		SpreadSheetTodoForm()
+    }
+}
