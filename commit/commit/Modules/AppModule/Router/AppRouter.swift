18dda6e32de7e7e73bab1dd9008636e1f1ebe0a5
Tomoya Tanaka <tomoya113.code@gmail.com>
2021-07-06 21:18:35 +0900

    add AppModule files

---
+++ commit/commit/Modules/AppModule/Router/AppRouter.swift
@@ -0,0 +1,8 @@
+//
+//  AppRouter.swift
+//  commit
+//
+//  Created by Tomoya Tanaka on 2021/07/06.
+//
+
+import Foundation
---
+++ commit/commit/Modules/AppModule/View/AppView.swift
@@ -0,0 +1,8 @@
+//
+//  AppView.swift
+//  commit
+//
+//  Created by Tomoya Tanaka on 2021/07/06.
+//
+
+import Foundation
