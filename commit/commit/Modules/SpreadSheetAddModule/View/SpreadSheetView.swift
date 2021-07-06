6acaa594f1104e1d0e662cc602ea50941da7b765
Tomoya Tanaka <tomoya113.code@gmail.com>
2021-07-06 20:52:38 +0900

    add URL Scheme

--- commit/commit/Modules/SpreadSheetAddModule/View/SpreadSheetView.swift
+++ commit/commit/Modules/SpreadSheetAddModule/View/SpreadSheetView.swift
@@ -8,9 +8,27 @@
 import SwiftUI
 
 struct SpreadSheetView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+	@State var text: String = ""
+	var body: some View {
+		Form {
+			Section(header: Text("スプレッドシート名")) {
+				TextField("タスク名", text: $text)
+			}
+			Section(header: Text("シート名")) {
+				TextField("シート名", text: $text)
+			}
+			Section(header: Text("セクション名")) {
+				TextField("セクション名", text: $text)
+			}
+			Section(header: Text("行")) {
+				TextField("行", text: $text)
+			}
+			Section(header: Text("列")) {
+				TextField("最初", text: $text)
+				TextField("最後", text: $text)
+			}
+		}
+	}
 }
 
 struct SpreadSheetView_Previews: PreviewProvider {
