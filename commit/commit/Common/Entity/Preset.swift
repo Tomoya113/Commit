59573646dd4ceccfdc36235a1ff6dc075468c8be
Tomoya Tanaka <tomoya113.code@gmail.com>
2021-07-10 23:10:55 +0900

    update SpreadSheetPreset Entity

--- commit/commit/Common/Entity/Preset.swift
+++ commit/commit/Common/Entity/Preset.swift
@@ -13,41 +13,32 @@ class Preset: Object, ObjectKeyIdentifiable {
 	@objc dynamic var sheetId: String = ""
 	@objc dynamic var title: String = ""
 	@objc dynamic var range: SheetRange?
+	// 書き込む行
+	@objc dynamic var targetRow: String = ""
 	
 	static override func primaryKey() -> String? {
 		return "id"
 	}
 	
-	convenience init( spreadSheetId: String, sheetId: String, title: String, range: SheetRange) {
+	convenience init( spreadSheetId: String, sheetId: String, title: String, range: SheetRange, targetRow: String) {
 		self.init()
 		self.spreadSheetId = spreadSheetId
 		self.sheetId = sheetId
 		self.title = title
 		self.range = range
+		self.targetRow = targetRow
 	}
 }
 
 class SheetRange: EmbeddedObject {
-	@objc dynamic var row: SheetRow?
+	@objc dynamic var row: String = ""
 	@objc dynamic var column: SheetColumn?
 	
-	convenience init(row: SheetRow, column: SheetColumn) {
+	convenience init(row: String, column: SheetColumn) {
 		self.init()
 		self.row = row
 		self.column = column
 	}
-}
-
-class SheetRow: EmbeddedObject {
-	@objc dynamic var start: String = ""
-	@objc dynamic var end: String = ""
-	
-	convenience init(start: String, end: String) {
-		self.init()
-		self.start = start
-		self.end = end
-	}
-	
 }
 
 class SheetColumn: EmbeddedObject {
