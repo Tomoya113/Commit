53e12b5beffe3b1254a4ac576296ebb93bb95401
Tomoya Tanaka <tomoya113.code@gmail.com>
2021-07-16 21:21:57 +0900

    add SubmitButton

---
+++ commit/commit/Common/Components/SubmitButton.swift
@@ -0,0 +1,43 @@
+//
+//  Button.swift
+//  commit
+//
+//  Created by Tomoya Tanaka on 2021/07/13.
+//
+
+import SwiftUI
+
+struct SubmitButton: View {
+	var action: () -> Void
+    var body: some View {
+		Button(action: {
+			action()
+		}, label: {
+			Text("更新")
+				.fontWeight(.bold)
+				.frame(minWidth: 0, maxWidth: .infinity)
+				.font(.system(size: 18))
+				.padding()
+				.foregroundColor(.white)
+				.background(Color.green)
+				.cornerRadius(10)
+		})
+		.padding(
+			EdgeInsets(
+				top: -8,
+				leading: 12,
+				bottom: 0,
+				trailing: 12)
+		)
+    }
+}
+
+struct SubmitButton_Previews: PreviewProvider {
+	static func test() {}
+	
+    static var previews: some View {
+		SubmitButton {
+			test()
+		}
+    }
+}
