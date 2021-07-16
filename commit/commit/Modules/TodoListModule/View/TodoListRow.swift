45be7bf765379ac11158d0c206156c4f422473ef
Tomoya Tanaka <tomoya113.code@gmail.com>
2021-07-16 21:25:25 +0900

    modify TodoListRow

--- commit/commit/Modules/TodoListModule/View/TodoListRow.swift
+++ commit/commit/Modules/TodoListModule/View/TodoListRow.swift
@@ -10,48 +10,73 @@ import RealmSwift
 
 struct TodoListRow: View {
 	@ObservedRealmObject var todo: Todo
-	let updateTodoStatus: ((String) -> Void)
-    var body: some View {
-		HStack {
+	let updateTodoStatus: () -> Void
+	var body: some View {
+		HStack(alignment: .center) {
 			VStack(alignment: .leading, spacing: 8) {
-				if todo.status!.finished {
-					Text(todo.title)
-						.strikethrough()
-						.foregroundColor(.gray)
-					Text(todo.subtitle)
-						.strikethrough()
-						.font(.subheadline).foregroundColor(.gray)
-				} else {
-					Text(todo.title)
-					Text(todo.subtitle)
-						.font(.subheadline).foregroundColor(.gray)
+				title()
+				if todo.subtitle != "" {
+					subtitle()
 				}
 			}
 			Spacer()
 			Button(action: {
-				updateTodoStatus(todo.id)
+				updateTodoStatus()
 			}, label: {
-				if todo.status!.finished {
-					Text(Image(systemName: "checkmark.square"))
-						.font(.system(size: 30))
-						.foregroundColor(.gray)
-				} else {
-					Text(Image(systemName: "circle"))
-						.font(.system(size: 30))
-						.foregroundColor(.gray)
-				}
+				Text(
+					Image(
+						systemName: todo.status!.finished ? "checkmark.square" : "circle"
+					)
+				)
+				.font(.system(size: 30))
+				.foregroundColor(.gray)
 			})
 			.buttonStyle(PlainButtonStyle())
 		}
-    }
+	}
+	
+	private func title() -> some View {
+		if todo.status!.finished {
+			return (
+				Text(todo.title)
+					.strikethrough()
+					.foregroundColor(.gray)
+			)
+		} else {
+			return Text(todo.title)
+		}
+	}
+	
+	private func subtitle() -> some View {
+		if todo.status!.finished {
+			return (
+				Text(todo.subtitle)
+					.strikethrough()
+					.font(.subheadline).foregroundColor(.gray)
+			)
+		} else {
+			return (
+				Text(todo.subtitle)
+					.font(.subheadline).foregroundColor(.gray)
+			)
+		}
+	}
 }
 
 struct TodoListRow_Previews: PreviewProvider {
-	static func test(id: String) {
-		print(id)
+	static func test(todo: Todo) {
+		print(todo)
 	}
-    static var previews: some View {
-		TodoListRow(todo: TodoMock.todoA1, updateTodoStatus: test)
-			.padding()
-    }
+	static var previews: some View {
+		VStack {
+			TodoListRow(todo: TodoMock.todoA1) {
+				test(todo: TodoMock.todoA1)
+			}
+				.padding()
+			TodoListRow(todo: TodoMock.todoWithoutDetail) {
+				test(todo: TodoMock.todoA1)
+			}
+				.padding()
+		}
+	}
 }
