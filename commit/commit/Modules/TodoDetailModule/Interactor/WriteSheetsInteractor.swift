3a157c427a42b93bbc669ba46350b6d571b99e8b
Tomoya Tanaka <tomoya113.code@gmail.com>
2021-08-26 11:09:14 +0900

    remove TodoRepository from WriteSheetsInteractor

--- commit/commit/Modules/TodoDetailModule/Interactor/WriteSheetsInteractor.swift
+++ commit/commit/Modules/TodoDetailModule/Interactor/WriteSheetsInteractor.swift
@@ -8,15 +8,8 @@
 import Foundation
 
 class WriteSheetsInteractor: UseCase {
-	let todoRepository: TodoRepositoryProtocol
 	let sheetsTodoAttributeRepository = RealmRepository<SheetsTodoAttribute>()
 	let sheetPresetRepository = RealmRepository<Preset>()
-	
-	init(
-		todoRepository: TodoRepositoryProtocol = TodoRepository.shared
-	) {
-		self.todoRepository = todoRepository
-	}
 	
 	func execute(_ parameters: Todo, completion: ((Result<Void, Never>) -> Void )?) {
 		if parameters.todoType != "googleSheets" {
