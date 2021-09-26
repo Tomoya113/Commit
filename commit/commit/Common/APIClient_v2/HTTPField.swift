2f58a75eb21503f03b9263ee86cb14b04128ed22
Tomoya Tanaka <tomoya113.code@gmail.com>
2021-09-26 09:58:03 +0900

    add HTTPField

---
+++ commit/commit/Common/APIClient_v2/HTTPField.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPField.swift
+//  commit
+//
+//  Created by Tomoya Tanaka on 2021/09/26.
+//
+
+import Foundation
+
+enum HTTPField: String {
+	// NOTE: 使ってるやつだけ
+	case AUTHORIZATION = "Authorization"
+	case CONTENT_TYPE = "Content-Type"
+	case ACCEPT = "Accept"
+}
